a5e0cb1a1b8424ac600acc78f098998d7d932a1c
Ben Guo <benzguo@gmail.com>
2020-08-09 15:36:47 -0700

    add favicons

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,11 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
         <body>
           <Main />
           <NextScript />
