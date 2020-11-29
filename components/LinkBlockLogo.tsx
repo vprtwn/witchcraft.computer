e166ee6c737681ebf09879bbfc9f099733361ebb
Ben Guo <benzguo@gmail.com>
2020-11-29 14:02:44 -0800

    use next/image fixes #107

--- components/LinkBlockLogo.tsx
+++ components/LinkBlockLogo.tsx
@@ -1,9 +1,10 @@
 import { linkStyleForUrl } from '../lib/utils';
+import Image from 'next/image';
 
 const LinkBlockLogo = (props) => {
   const linkStyle = linkStyleForUrl(props.url);
   if (linkStyle.logo) {
-    return <img src={`/svg/${linkStyle.logo}.svg`} width="16px" />;
+    return <Image src={`/svg/${linkStyle.logo}.svg`} alt={linkStyle.logo} width="16px" height="16px" />;
   } else {
     return <></>;
   }
