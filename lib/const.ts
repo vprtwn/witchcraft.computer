81caa2813f0a64e400b1a4969e7c1a8b1d6fe517
Andrew <healeycodes@gmail.com>
2020-12-08 18:35:41 +0000

    Add size argument for favicons

--- lib/const.ts
+++ lib/const.ts
@@ -5,3 +5,4 @@ export const FONT_MONO =
   'Recursive, San Francisco Mono, Monaco, "Consolas", "Lucida Console", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", monospace';
 export const FONT_SANS =
   '-apple-system, BlinkMacSystemFont, "avenir next", avenir, helvetica, "helvetica neue", ubuntu, roboto, noto, "segoe ui", arial, sans-serif';
+export const GOOGLE_FAVICONS = 'https://s2.googleusercontent.com/s2/favicons'
