847a20c3b8f50141c961d4f14e700b1b7afe7415
Ben Guo <benzguo@gmail.com>
2020-12-08 08:48:13 -0800

    make user links black

--- components/UserLink.tsx
+++ components/UserLink.tsx
@@ -6,8 +6,8 @@ const UserLink = (props) => {
 
   return (
     <Flex key={username} sx={{ py: 2, alignItems: 'center', justifyContent: 'center' }}>
-      <Card sx={{ border: 'solid 1px gray', borderRadius: 4, px: 2, py: 0, alignItems: 'center' }}>
-        <Link variant="link_no_underline" sx={{ color: 'gray', '&:visited': { color: 'gray' } }} href={`/@${username}`}>
+      <Card sx={{ border: 'solid 1px black', borderRadius: 4, px: 2, py: 0, alignItems: 'center' }}>
+        <Link variant="link_no_underline" href={`/@${username}`}>
           {`@${username}`}
         </Link>
       </Card>
